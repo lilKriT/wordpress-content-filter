2b562d696dbd71866e71c612fc433c0bc73170cc
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-22 18:17:25 +0100

    Created a simple plugin page.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 
 /*
     Plugin Name: Content Filter
@@ -8,4 +8,25 @@
     Author URI: https://lilKriT.dev
 */
 
-if( !defined('ABSPATH')) exit;  // Don't allow to access directly.
+if (!defined('ABSPATH')) exit;  // Don't allow to access directly.
+
+class ContentFilter
+{
+    function __construct()
+    {
+        add_action("admin_menu", array($this, "addMenu"));
+    }
+
+    function addMenu()
+    {
+        // Document title, admin sidebar name, permissions you need, slug, html function, icon, where does it appear (like priority. bigger number, lower position)
+        add_menu_page("Words to Filter", "Word Filter", "manage_options", "wordfilter", array($this, "wordFilterPage"), "dashicons-smiley", 100);
+    }
+
+    function wordFilterPage()
+    { ?>
+        Hello Plugin
+<?php }
+}
+
+$contentFilter = new ContentFilter();
