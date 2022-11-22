0446343c1cfb6d703f4a7401ad8374cbaed40e37
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-22 18:20:33 +0100

    Added a submenu.

--- index.php
+++ index.php
@@ -21,11 +21,18 @@ class ContentFilter
     {
         // Document title, admin sidebar name, permissions you need, slug, html function, icon, where does it appear (like priority. bigger number, lower position)
         add_menu_page("Words to Filter", "Word Filter", "manage_options", "wordfilter", array($this, "wordFilterPage"), "dashicons-smiley", 100);
+        // menu to add to, document title, sidebar text, permissions, slug, html function
+        add_submenu_page("wordfilter", "Word Filter Options", "Options", "manage_options", "wordfilter-options", array($this, "optionsSubPage"));
     }
 
     function wordFilterPage()
     { ?>
         Hello Plugin
+    <?php }
+
+    function optionsSubPage()
+    { ?>
+        Hello Subpage
 <?php }
 }
 
