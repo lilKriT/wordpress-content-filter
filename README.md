a9797ebc406f0a0dbc3e3c68e0704f878bc2bdc9
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-22 18:07:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wordpress Content Filter
+
+A simple plugin in which you can provide a list of words to be filtered. Following a Udemy tutorial.
+Created by lilKriT.
---
+++ index.php
@@ -0,0 +1,11 @@
+<? php
+
+/*
+    Plugin Name: Content Filter
+    Description: Finds and replaces a provided list of words.
+    Version: 1.0
+    Author: lilKriT
+    Author URI: https://lilKriT.dev
+*/
+
+if( !defined('ABSPATH')) exit;  // Don't allow to access directly.
